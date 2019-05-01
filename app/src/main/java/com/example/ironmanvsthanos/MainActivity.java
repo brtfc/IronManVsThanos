c707f5a66ae80e6067582d237882f940d2a9ef67
brtfc <adomprince@gmail.com>
2019-05-01 18:32:21 -0400

    color values, splash screen no action bar, audio sound on view click

--- app/src/main/java/com/example/ironmanvsthanos/MainActivity.java
+++ app/src/main/java/com/example/ironmanvsthanos/MainActivity.java
@@ -1,13 +1,29 @@
 package com.example.ironmanvsthanos;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    Handler handler;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
         setContentView(R.layout.activity_main);
+
+        //setTheme(R.style.AppTheme);
+        handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, GamePlay.class);
+                startActivity(intent);
+            }
+        }, 6000);
     }
 }
